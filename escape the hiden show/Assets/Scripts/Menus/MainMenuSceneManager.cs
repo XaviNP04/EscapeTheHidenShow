2053fa1e32a6dea87f036d34076eedfc4723a511
LookinFerPaperClips <115730833+mheilac030@users.noreply.github.com>
2025-12-02 21:26:19 +0100

    Icono para dial en inventario
    + Ajustado el Splash + Script del Main Menu
    + Ajustado el icono de llave para que se parezca al del dial
    + Añadido luz al fondo del tunel

--- escape the hiden show/Assets/Scripts/Menus/MainMenuSceneManager.cs
+++ escape the hiden show/Assets/Scripts/Menus/MainMenuSceneManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenuSceneManager : MonoBehaviour
@@ -10,15 +10,26 @@ public class MainMenuSceneManager : MonoBehaviour
     [SerializeField] private Image fadeImage;
     [SerializeField] private float fadeDuration = 1f;
 
-    public Button defaultButton;
+    [SerializeField] private GameObject creditsPanel;
 
-     void Start()
+    private bool isTransitioning = false;
+
+    void Update()
     {
-        EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
+        if (!creditsPanel.activeInHierarchy) {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                OnPlayButton();
+       
+        } else
+            EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void OnPlayButton()
     {
+        if (isTransitioning) return;
+
+        isTransitioning = true;
+
         StartCoroutine(TransitionRoutine());
     }
 
